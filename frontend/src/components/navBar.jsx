3e427b09fd5246d68492c9157f42c6a1af10c375
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2023-04-10 06:40:16 +0000

    zynocodes

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,11 +1,13 @@
 import { FaUserMd } from "react-icons/fa";
 import { BsBell } from "react-icons/bs";
-import MyAsideBarActive from "./asideBarActive";
-import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+
 export default function MyNavBar({ act, setAct }) {
   const classToggle = () => {
     setAct(!act);
   };
+  const { user } = useAuthContext();
   return (
     <nav>
       <div className="left-nav">
@@ -24,12 +26,12 @@ export default function MyNavBar({ act, setAct }) {
         </a>
       </div>
       <div className="right-nav">
-        <BsBell size="24px" cursor="pointer" />
+        <BsBell size="24px" cursor="pointer"/>
         <div className="doctor">
           <div className="doctor-pic"></div>
           <div className="doctor-name">
             {user && (
-              <a className="medcine-name" href="#">Dr. {user.Fname}</a>
+              <a className="medcine-name">Dr. {user.Fname}</a>
             )}
             {user && (
               <span>{user.speciality}</span>
