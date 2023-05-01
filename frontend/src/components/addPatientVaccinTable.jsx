df0e7bce297552ad7c11dd17812af4987fb32db2
moumen26 <moumenkhaldi26@gmail.com>
2023-05-01 15:08:53 +0100

    table vaccin css

--- frontend/src/components/addPatientVaccinTable.jsx
+++ frontend/src/components/addPatientVaccinTable.jsx
@@ -23,7 +23,6 @@ export default function VaccinTable() {
     vaccinationNumero: "",
     vaccinationDate: "",
   });
-  
 
   const [editVaccinId, setEditVaccinId] = useState(null);
 
@@ -127,124 +126,135 @@ export default function VaccinTable() {
               setaddVaccinTable={setaddVaccinTable}
             />
           </div>
-          <table className="vaccination-table">
-            <tr>
-              <th>
-                <span>Age</span>
-              </th>
-              <th>
-                <span>Vaccin</span>
-              </th>
-              <th>
-                <span>Contre</span>
-              </th>
-              <th>
-                <span>
-                  Technique
-                  <br />
-                  vaccinale
-                </span>
-              </th>
-              <th>
-                <span>
-                  Numero du
-                  <br />
-                  lot
-                </span>
-              </th>
-              <th>
-                <span>Date</span>
-              </th>
-            </tr>
-          </table>
+          <div className="vaccination-table-add">
+            <div className="add-vaccin-table-item">
+              <span>Age</span>
+            </div>
+            <div className="add-vaccin-table-item l-border">
+              <span>Vaccin</span>
+            </div>
+            <div className="add-vaccin-table-item l-border">
+              <span>Contre</span>
+            </div>
+            <div className="add-vaccin-table-item l-border">
+              <span>
+                Technique
+                <br />
+                vaccinale
+              </span>
+            </div>
+            <div className="add-vaccin-table-item l-border r-border">
+              <span>
+                Numero du
+                <br />
+                lot
+              </span>
+            </div>
+            <div className="add-vaccin-table-item">
+              <span>Date</span>
+            </div>
+          </div>
           <form onSubmit={handleAddFormSubmit}>
-            <div className="form-table">
-              <select
-                className="vaccination-select"
-                name="vaccinationAge"
-                id="vaccination-age"
-                required="required"
-                onChange={handleAddFormChange}
-              >
-                <option selected disabled>
-                  Age
-                </option>
+            <div className="form-table2">
+              <div className="form-table-item">
+                <select
+                  className="vaccination-select"
+                  name="vaccinationAge"
+                  id="vaccination-age"
+                  required="required"
+                  onChange={handleAddFormChange}
+                >
+                  <option selected disabled>
+                    Age
+                  </option>
 
-                <option value="1mois">1 mois</option>
-                <option value="2mois">2 mois</option>
-                <option value="3mois">3 mois</option>
-              </select>
+                  <option value="1mois">1 mois</option>
+                  <option value="2mois">2 mois</option>
+                  <option value="3mois">3 mois</option>
+                </select>
+              </div>
 
-              <select
-                className="vaccination-select"
-                name="vaccinationVaccin"
-                id="vaccination-vaccin"
-                required="required"
-                onChange={handleAddFormChange}
-              >
-                <option selected disabled>
-                  Vaccin
-                </option>
-                <option value="vaccin1">vaccin</option>
-                <option value="vaccin2">vaccin2</option>
-                <option value="vaccin3">vaccin3</option>
-                <option value="vaccin4">vaccin4</option>
-              </select>
+              <div className="form-table-item l-border">
+                <select
+                  className="vaccination-select"
+                  name="vaccinationVaccin"
+                  id="vaccination-vaccin"
+                  required="required"
+                  onChange={handleAddFormChange}
+                >
+                  <option selected disabled>
+                    Vaccin
+                  </option>
+                  <option value="vaccin1">vaccin</option>
+                  <option value="vaccin2">vaccin2</option>
+                  <option value="vaccin3">vaccin3</option>
+                  <option value="vaccin4">vaccin4</option>
+                </select>
+              </div>
 
-              <select
-                className="vaccination-select"
-                name="vaccinationContre"
-                id="vaccination-contre"
-                required="required"
-                onChange={handleAddFormChange}
-              >
-                <option selected disabled>
-                  Contre
-                </option>
-                <option value="contre1">contre</option>
-                <option value="contre2">contre2</option>
-                <option value="contre3">contre3</option>
-                <option value="contre4">contre4</option>
-              </select>
+              <div className="form-table-item l-border">
+                <select
+                  className="vaccination-select"
+                  name="vaccinationContre"
+                  id="vaccination-contre"
+                  required="required"
+                  onChange={handleAddFormChange}
+                >
+                  <option selected disabled>
+                    Contre
+                  </option>
+                  <option value="contre1">contre</option>
+                  <option value="contre2">contre2</option>
+                  <option value="contre3">contre3</option>
+                  <option value="contre4">contre4</option>
+                </select>
+              </div>
 
-              <select
-                className="vaccination-select"
-                name="vaccinationTechnique"
-                id="vaccination-technique"
-                required="required"
-                onChange={handleAddFormChange}
-              >
-                <option selected disabled>
-                  Technique
-                </option>
-                <option value="technique1">technique1</option>
-                <option value="technique2">technique2</option>
-                <option value="technique3">technique3</option>
-                <option value="technique4">technique4</option>
-              </select>
+              <div className="form-table-item l-border">
+                <select
+                  className="vaccination-select"
+                  name="vaccinationTechnique"
+                  id="vaccination-technique"
+                  required="required"
+                  onChange={handleAddFormChange}
+                >
+                  <option selected disabled>
+                    Technique
+                  </option>
+                  <option value="technique1">technique1</option>
+                  <option value="technique2">technique2</option>
+                  <option value="technique3">technique3</option>
+                  <option value="technique4">technique4</option>
+                </select>
+              </div>
 
-              <select
-                className="vaccination-select"
-                name="vaccinationNumero"
-                id="vaccination-numero"
-                required="required"
-                onChange={handleAddFormChange}
-              >
-                <option selected disabled>
-                  Numero
-                </option>
-                <option value="numero1">numero1</option>
-                <option value="numero2">numero2</option>
-                <option value="numero3">numero3</option>
-                <option value="numero4">numero4</option>
-              </select>
+              <div className="form-table-item l-border r-border">
+                <select
+                  className="vaccination-select"
+                  name="vaccinationNumero"
+                  id="vaccination-numero"
+                  required="required"
+                  onChange={handleAddFormChange}
+                >
+                  <option selected disabled>
+                    Numero
+                  </option>
+                  <option value="numero1">numero1</option>
+                  <option value="numero2">numero2</option>
+                  <option value="numero3">numero3</option>
+                  <option value="numero4">numero4</option>
+                </select>
+              </div>
 
-              <input
-                type="date"
-                name="vaccinationDate"
-                required="required"
-                onChange={handleAddFormChange}
-              />
+              <div className="form-table-item s-table-item">
+                <input
+                  className="vaccination-input"
+                  type="date"
+                  name="vaccinationDate"
+                  required="required"
+                  onChange={handleAddFormChange}
+                />
+              </div>
             </div>
 
             <div className="add-tableau-vaccin-ajouter-btn">
