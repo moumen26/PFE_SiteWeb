d8459d55b599f6f49d4ec47cbeee43a493ab6b8e
moumen26 <moumenkhaldi26@gmail.com>
2023-05-27 00:10:47 +0100

    aside item active

--- frontend/src/components/asideBar.jsx
+++ frontend/src/components/asideBar.jsx
@@ -7,7 +7,7 @@ import { IoIosSettings } from "react-icons/io";
 import { FiLogOut } from "react-icons/fi";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function MyAsideBar() {
   const { logout } = useLogout();
@@ -16,13 +16,19 @@ export default function MyAsideBar() {
   const SubmitLogout = () => {
     logout();
   };
+
+  const location = useLocation();
   return (
     <div className="asidebar">
       <aside className="aside">
         <ul>
           <li className="mb-6">
             <NavLink to="/">
-              <div className="link flex items-center justify-items-center">
+              <div
+                className={`link flex items-center justify-items-center ${
+                  location.pathname === "/" ? "aside-item-active" : ""
+                }`}
+              >
                 <MdDashboard className="w-6 h-6 ml-2" />
                 <span className="title">Dashboard</span>
               </div>
@@ -30,7 +36,11 @@ export default function MyAsideBar() {
           </li>
           <li className="mb-6">
             <NavLink to="patients">
-              <div className="link flex items-center ">
+              <div
+                className={`link flex items-center ${
+                  location.pathname === "/patients" ? "aside-item-active" : ""
+                }`}
+              >
                 <BsFilePersonFill className="w-6 h-6 ml-2 " />
                 <span className="title">Patients</span>
               </div>
@@ -38,7 +48,11 @@ export default function MyAsideBar() {
           </li>
           <li className="mb-6">
             <NavLink to="Nouveaune">
-              <div className="link flex items-center ">
+              <div
+                className={`link flex items-center ${
+                  location.pathname === "/Nouveaune" ? "aside-item-active" : ""
+                }`}
+              >
                 <BsFilePerson className="w-6 h-6 ml-2 " />
                 <span className="title">Nouveau-ne</span>
               </div>
@@ -46,7 +60,11 @@ export default function MyAsideBar() {
           </li>
           <li className="mb-6">
             <NavLink to="calendrier">
-              <div className="link flex items-center ">
+              <div
+                className={`link flex items-center ${
+                  location.pathname === "/calendrier" ? "aside-item-active" : ""
+                }`}
+              >
                 <BsFillCalendarFill className="w-6 h-6 ml-2 " />
                 <span className="title">Calendrier</span>
               </div>
@@ -54,7 +72,11 @@ export default function MyAsideBar() {
           </li>
           <li className="mb-6">
             <NavLink to="affichage">
-              <div className="link flex items-center ">
+              <div
+                className={`link flex items-center ${
+                  location.pathname === "/affichage" ? "aside-item-active" : ""
+                }`}
+              >
                 <RiCoupon2Fill className="w-6 h-6 ml-2 " />
                 <span className="title">Coupon</span>
               </div>
@@ -62,7 +84,11 @@ export default function MyAsideBar() {
           </li>
           <li className="mb-6">
             <NavLink to="analytics">
-              <div className="link flex items-center ">
+              <div
+                className={`link flex items-center ${
+                  location.pathname === "/analytics" ? "aside-item-active" : ""
+                }`}
+              >
                 <MdAnalytics className="w-6 h-6 ml-2 " />
                 <span className="title">Analytique</span>
               </div>
@@ -70,7 +96,11 @@ export default function MyAsideBar() {
           </li>
           <li>
             <NavLink to="conculter">
-              <div className="link flex items-center ">
+              <div
+                className={`link flex items-center ${
+                  location.pathname === "/conculter" ? "aside-item-active" : ""
+                }`}
+              >
                 <IoIosSettings className="w-6 h-6 ml-2" />
                 <span className="title">Paramètres</span>
               </div>
@@ -79,7 +109,10 @@ export default function MyAsideBar() {
           <li>
             <NavLink to="login">
               {user && (
-                <div className="link flex items-center " onClick={SubmitLogout}>
+                <div
+                  className="link flex items-center"
+                  onClick={SubmitLogout}
+                >
                   <FiLogOut className="w-6 h-6 ml-2" />
                   <span className="title">Log Out</span>
                 </div>
